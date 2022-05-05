12d8f04ef7459f1d2d96ee94f353db10bf587126
Oudomphon <62913907+Oudomphon@users.noreply.github.com>
2022-05-05 10:35:55 +0700

    Update HW03_Extern2.c

--- HW03_Extern2.c
+++ HW03_Extern2.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 extern int L,l,Area;
-void rectangle(void){
+void HW03_Extern2(void){
 printf("Wanna find area of rectangle? \n Pls tell me the length is \n");
 scanf("%d",&L);
 printf("The width is \n");
