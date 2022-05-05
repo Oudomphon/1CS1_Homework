d8a8f92073d9f124d422c4eac5dce196916bc6b9
Oudomphon <62913907+Oudomphon@users.noreply.github.com>
2022-05-05 10:38:13 +0700

    Create HW02_ASCII.c

---
+++ HW02_ASCII.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  char a;
+  printf("Please insert a character: (^o^)\n");
+  scanf("%c",&a);
+  printf("The ASCII value of the character is %d",a);
+  return 0;
+}
